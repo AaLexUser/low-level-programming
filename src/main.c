9650687f1113fd4d39f1ec4fd7535e1693f269d8
Alexey Lapin <333893@niuitmo.ru>
2023-10-11 20:58:46 +0300

    change some lines in main

--- src/main.c
+++ src/main.c
@@ -18,23 +18,6 @@ int main(int argc, char* argv[]) {
     schema_add_char_field(schema, "name", 3);
     schema_add_float_field(schema, "score");
     schema_add_bool_field(schema, "pass");
-
-    Table *table = create_table("test", schema);
-    void* data = malloc(schema->slot_size);
-    int64_t id = 1;
-    size_t offset = 0;
-    memcpy(data, &id, sizeof(int64_t));
-    offset += sizeof(int64_t);
-    char* name = "aaa";
-    strncpy(data + offset, name, sizeof(char) * 3);
-    offset += sizeof(char) * 4;
-    float score = 1.1f;
-    memcpy(data + offset, &score, sizeof(float));
-    offset += sizeof(float);
-    bool pass = true;
-    memcpy(data + offset, &pass, sizeof(bool));
-    table_insert(table, data);
-    free(data);
-    table_select(table);
-    free_table(table);
+    table_manager_add_table_inplace(db->table_manager, "test", schema);
+    Table* table = table_manager_get_table(db->table_manager, "test");
 }
